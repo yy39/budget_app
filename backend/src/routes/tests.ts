08ecc27443e4e2fd4da386cb5e2d9771e5362f23
Yusan Yin <yusanyin@gmail.com>
2023-10-16 13:53:49 -0700

    add pathLogger middleware

--- backend/src/routes/tests.ts
+++ backend/src/routes/tests.ts
@@ -1,6 +1,15 @@
 import { Elysia } from "elysia"
+import type { Handler, Context } from "elysia"
+
+const contextHandler: Handler = (context: Context) => {
+  return context
+}
+
+const pathLogger = (context: any) => {
+  console.log(context.path)
+}
 
 export const testRoutes = new Elysia({ prefix: "/tests" })
   .get("/", () => "test")
   .get("/:stuff", ({ params: { stuff } }) => stuff)
-  .get("/context", (context) => context)
+  .get("/context", (context) => contextHandler(context), { beforeHandle: pathLogger})
